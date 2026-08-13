31d6653189ea5a351441d85c9361125600f6e3f8
grah4na <syknandan@gmail.com>
2026-08-13 10:00:00 +0000

    chore: bootstrap prism python project

---
+++ prism/__init__.py
@@ -0,0 +1,8 @@
+"""Prism - fresh take on HTTP differential testing.
+
+Inspired by HTTP Prism (GPL-3.0). All code below is written from scratch
+to produce compatible output; see ../README.md and LICENSE for upstream credit.
+This package is also GPL-3.0.
+"""
+
+__version__ = "0.1.0"
